9e3bbf4df8946adbdcdf8255414006299582ae5a
amarnath <tanath2007@gmail.com>
2025-04-02 11:41:08 +0530

    first commit

--- Downloads/stackblitz-starters-amh9afad/index.js
+++ Downloads/stackblitz-starters-amh9afad/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose=require("mongoose")
+require('dotenv').config()
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
-
+const db = async ()=>{
+  try {
+  await  mongoose.connect(process.env.URI)
+   console.log("connected successfully")
+ } catch (error) {
+   console.log(error.message)
+}
+}
+db()
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
